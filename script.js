da6c7016a5e04eb34aab9e98064ad2f303b01087
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-09-12 12:22:31 +0200

    Project started

---
+++ script.js
@@ -0,0 +1,11 @@
+let points = 'julie';
+
+function changeChar(){
+    points.charAt(0).toUpperCase();
+    document.getElementById("points").innerHTML = points;
+}
+console.loh(changeChar);
+
+// let result = myName.charAt(0).toUpperCase();
+
+// console.log(result);
